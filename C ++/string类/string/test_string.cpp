b42d08ca7ea28d6f1ff7a095a7ed9c396f7cf719
achen228 <achen228@163.com>
2019-10-16 18:49:51 +0800

    Update test_string.cpp

--- C ++/string类/string/test_string.cpp
+++ C ++/string类/string/test_string.cpp
@@ -38,3 +38,67 @@ void test_string2()
 	s += w;
 	cout << s << endl;
 }
+
+
+void test_string3() 
+{
+	//string s("hello");
+	//// 一般使用 size
+	//cout << s.size() << endl;
+	//cout << s.length() << endl;
+	//cout << s.max_size() << endl;
+
+	//s += "xxxxxxxxxxxxxxxxxxx";
+	//cout << s.size() << endl;
+	//cout << s.capacity() << endl;
+	//s.clear(); // 清空数据
+	//cout << s << endl;
+	//cout << s.size() << endl;
+	//cout << s.capacity() << endl;
+
+
+	//resize()，设置大小（size）;
+	//reserve()，设置容量（capacity）;
+	//size()是分配容器的内存大小，而capacity()只是设置容器容量大小，但并没有真正分配内存。
+	//打个比方：正在建造的一辆公交车，车里面可以设置40个座椅（reserve(40); ），这是它的容量，
+	//但并不是说它里面就有了40个座椅，只能说明这部车内部空间大小可以放得下40张座椅而已。
+	//而车里面安装了40个座椅(resize(40);)，这个时候车里面才真正有了40个座椅，这些座椅就可以使用了
+
+
+	//string s("hello");
+	//s.resize(13);
+	//s.resize(20, 'a');
+	//cout << s << endl;
+	//cout << s.c_str() << endl;
+	//cout << s.size() << endl;
+	//cout << s.capacity() << endl;
+
+
+	//string s("hello");
+	//s.reserve(10);
+	//s.reserve(20);
+	//cout << s << endl;
+	//cout << s.size() << endl;
+	//cout << s.capacity() << endl;
+
+
+	// capacity 大约以 size 的1.5倍增长
+	string s;
+	s.reserve(1000);
+	s.resize(1000);
+	size_t sz = s.capacity();
+	cout << "capacity changed: " << sz << '\n';
+	cout << "making s grow:\n";
+	for (int i = 0; i < 1000; ++i) 
+	{
+		s += 'c';
+		//s[i] = 'c';
+		if (sz != s.capacity()) 
+		{
+			sz = s.capacity();
+			cout << "capacity changed: " << sz << '\n';
+		}
+	}
+}
+
+
