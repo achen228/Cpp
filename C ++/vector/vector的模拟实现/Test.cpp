cadf82c5df7cbf26aca2569aba261a217453e4f5
achen228 <achen228@163.com>
2019-11-04 16:40:07 +0800

    Update Test.cpp

--- C ++/vector/vector的模拟实现/Test.cpp
+++ C ++/vector/vector的模拟实现/Test.cpp
@@ -30,3 +30,33 @@ void TestVector1()
 	}
 	cout << endl;
 }
+
+
+void TestVector2()
+{
+	Vector<int> v;
+	v.PushBack(1);
+	v.PushBack(2);
+	v.PushBack(3);
+	v.PushBack(4);
+	v.PushBack(5);
+	v.PushBack(6);
+	v.PushBack(7);
+
+	Vector<int>::iterator it = v.begin();
+	v.Insert(it, 0);
+	Vector<int>::iterator ret = std::find(v.begin(), v.end(), 3);
+	if (ret != v.end())
+	{
+		v.Insert(ret, 30);
+	}
+
+	ret = std::find(v.begin(), v.end(), 3);
+	v.Erase(ret);
+
+	for (auto e : v) 
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+}
