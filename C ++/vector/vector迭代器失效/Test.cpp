b5a32fb47d9638b0cd6638cb92c5bbf06a9f7d25
achen228 <achen228@163.com>
2019-11-09 14:24:12 +0800

    Update Test.cpp

--- C ++/vector/vector迭代器失效/Test.cpp
+++ C ++/vector/vector迭代器失效/Test.cpp
@@ -22,3 +22,41 @@ void test_vector1()
 	v.insert(ret, 20);
 	cout << v.capacity() << endl;
 }
+
+
+void test_vector2() 
+{
+	std::vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(2);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+
+	auto it = v.begin();
+	while (it != v.end()) 
+	{
+		if (*it % 2 == 0) 
+		{
+			it = v.erase(it);
+		}
+		else 
+		{
+			++it;
+		}
+	}
+
+	for (auto e : v)
+	{
+		cout << e << endl;
+	}
+}
+
+int main()
+{
+	test_vector2();
+
+	system("pause");
+	return 0;
+}
