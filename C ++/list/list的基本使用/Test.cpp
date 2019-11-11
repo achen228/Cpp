dfc5131f038a400ff05a91a660916361ba2acb85
achen228 <achen228@163.com>
2019-11-11 19:55:37 +0800

    Update Test.cpp

--- C ++/list/list的基本使用/Test.cpp
+++ C ++/list/list的基本使用/Test.cpp
@@ -26,3 +26,39 @@ void test_list1()
 	}
 	cout << endl;
 }
+
+
+void test_list2() 
+{
+	list<int> l;
+	l.push_back(1);
+	l.push_back(2);
+	l.push_back(3);
+	l.push_back(4);
+
+	list<int>::iterator pos = std::find(l.begin(), l.end(), 3);
+	if (pos != l.end())
+	{
+		l.insert(pos, 30);
+	}
+	for (auto e : l)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	pos = std::find(l.begin(), l.end(), 3);
+	if (pos != l.end()) 
+	{
+		l.erase(pos);
+	}
+	for (auto e : l)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	//// erase以后迭代器失效，不要再去访问pos
+	//++pos;
+	//cout << *pos << endl;
+}
