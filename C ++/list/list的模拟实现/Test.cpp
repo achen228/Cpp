3ae57c925753436aade6beb55905b34d07b134bd
achen228 <achen228@163.com>
2019-11-17 15:47:39 +0800

    Update Test.cpp

--- C ++/list/list的模拟实现/Test.cpp
+++ C ++/list/list的模拟实现/Test.cpp
@@ -21,3 +21,27 @@ void TestList1()
 	}
 	cout << endl;
 }
+
+
+void TestList2()
+{
+	List<AA> laa;
+	AA aa1 = { 1, 2 };
+	laa.PushBack(aa1);
+	laa.PushBack({ 3, 4 });
+
+	List<AA>::iterator ita = laa.begin();
+	while (ita != laa.end())
+	{
+		//cout << (*ita)._a1 << ' ' << (*ita)._a2 << endl;
+		cout << ita->_a1 << ' ' << ita->_a2 << endl;
+		++ita;
+	}
+
+	AA* pa = new AA;
+	pa->_a1 = 10;
+	cout << (*pa)._a1 << endl;
+
+	int* pi = new int;
+	cout << *pi << endl;
+}
