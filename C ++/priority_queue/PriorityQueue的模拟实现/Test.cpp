e7dbd9b732f159e375dfee701643a0ce8960d34c
achen228 <achen228@163.com>
2019-11-29 16:43:07 +0800

    Update Test.cpp

--- C ++/priority_queue/PriorityQueue的模拟实现/Test.cpp
+++ C ++/priority_queue/PriorityQueue的模拟实现/Test.cpp
@@ -11,3 +11,27 @@ void TestPriorityQueue1()
 	cout << Less<int>()(1, 3) << endl;
 	cout << greater(1, 3) << endl;
 }
+
+
+void TestPriorityQueue2()
+{
+	PriorityQueue<int, vector<int>, Less<int>> pq1;
+	pq1.Push(4);
+	pq1.Push(3);
+	pq1.Push(1);
+	pq1.Push(9);
+	pq1.Push(7);
+	while (!pq1.Empty())
+	{
+		cout << pq1.Top() << endl;
+		pq1.Pop();
+	}
+}
+
+int main()
+{
+	TestPriorityQueue2();
+
+	system("pause");
+	return 0;
+}
