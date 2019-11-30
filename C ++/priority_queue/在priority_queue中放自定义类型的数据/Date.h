8793d3cb5836392b01542aa0c803ac28bca100d9
achen228 <achen228@163.com>
2019-11-30 17:18:16 +0800

    Create Date.h

---
+++ C ++/priority_queue/在priority_queue中放自定义类型的数据/Date.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+		: _year(year)
+		, _month(month)
+		, _day(day)
+	{}
+
+	bool operator<(const Date& d)const
+	{
+		return (_year < d._year) ||
+			(_year == d._year && _month < d._month) ||
+			(_year == d._year && _month == d._month && _day < d._day);
+	}
+
+	bool operator>(const Date& d)const
+	{
+		return (_year > d._year) ||
+			(_year == d._year && _month > d._month) ||
+			(_year == d._year && _month == d._month && _day > d._day);
+	}
+
+	friend ostream& operator<<(ostream& _cout, const Date& d)
+	{
+		_cout << d._year << "-" << d._month << "-" << d._day;
+		return _cout;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
