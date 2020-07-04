4f0f0d1525cbd4aaf61d4b363b226894711b804f
achen228 <achen228@163.com>
2020-07-04 13:59:30 +0800

    Update List.h

--- STL/list/list的模拟实现/List.h
+++ STL/list/list的模拟实现/List.h
@@ -1,242 +1,233 @@
-// list 的模拟实现
 #pragma once
 #include <iostream>
-#include <assert.h>
+#include <cassert>
 using namespace std;
 
-template<class T>
-struct ListNode
+namespace MakeList
 {
-	ListNode<T>* _next;
-	ListNode<T>* _prev;
-	T _data;
-
-	ListNode(const T& x = T())
-		:_next(nullptr)
-		, _prev(nullptr)
-		, _data(x)
-	{}
-};
-
-template<class T, class Ref, class Ptr>
-struct __ListIterator 
-{
-	typedef ListNode<T> Node;
-	typedef __ListIterator<T, Ref, Ptr> Self;
-	Node* _node;
-
-	__ListIterator(Node* node)
-		:_node(node)
-	{}
-
-	// *it;
-	Ref operator*() 
+	template<class T>
+	struct ListNode
 	{
-		return _node->_data;
-	}
+		ListNode* _prev;
+		ListNode* _next;
+		T _data;
+		ListNode(const T& data = T())
+			: _prev(nullptr)
+			, _next(nullptr)
+			, _data(data)
+		{}
+	};
 
-	// it->
-	Ptr operator->() 
+	template<class T, class Ref, class Ptr>
+	struct ListIterator
 	{
-		return &_node->_data;
-	}
+		typedef ListIterator<T, Ref, Ptr> Self;
+		typedef ListNode<T> Node;
+		Node* _node;
 
-	Self& operator++()  // 前置++
-	{
-		_node = _node->_next;
-		return *this;
-	}
+		ListIterator(Node* node)
+			: _node(node)
+		{}
 
-	Self operator++(int)  // 后置++
-	{
-		Self tmp(*this);
-		_node = _node->_next;
-		return tmp;
-	}
-
-	Self& operator--()  // 前置--
-	{
-		_node = _node->_prev;
-		return *this;
-	}
-
-	Self operator--(int)  // 后置--
-	{
-		Self tmp(*this);
-		_node = _node->_prev;
-		return tmp;
-	}
-
-	// it1 != it2
-	bool operator!=(const Self& it) 
-	{
-		return _node != it._node;
-	}
-
-	// it1 == it2
-	bool operator==(const Self& it)
-	{
-		return _node == it._node;
-	}
-};
-
-
-template<class T>
-class List 
-{
-	typedef ListNode<T> Node;
-public:
-	typedef __ListIterator<T, T&, T*> iterator;
-	typedef __ListIterator<T, const T&, const T*> const_iterator;
-
-	List()
-		:_head(new Node)
-	{
-		_head->_next = _head;
-		_head->_prev = _head;
-	}
-
-	// copy(l)  拷贝构造
-	List(const List<T>& l) 
-	{
-		_head = new Node;
-		_head->_next = _head;
-		_head->_prev = _head;
-
-		for (auto e : l)
+		Ref operator*()
 		{
-			PushBack(e);
+			return _node->_data;
 		}
-	}
 
-	// 赋值
-	List<T>& operator=(List<T> l)
-	{
-		swap(_head, l._head);
-		return *this;
-	}
-
-	~List()
-	{
-		Clear();
-		delete _head;
-		_head = nullptr;
-	}
-
-	// 清理数据
-	void Clear() 
-	{
-		iterator it = begin();
-		while (it != end())
+		Ptr operator->()
 		{
-			//it = Erase(it);
-			Erase(it++);
+			return &_node->_data;
 		}
-	}
 
-	iterator begin()
-	{
-		return iterator(_head->_next);
-	}
-
-	iterator end()
-	{
-		return iterator(_head);
-	}
-
-	const_iterator begin() const
-	{
-		return const_iterator(_head->_next);
-	}
-
-	const_iterator end() const
-	{
-		return const_iterator(_head);
-	}
-
-	// 尾插
-	void PushBack(const T& x) 
-	{
-		Node* tail = _head->_prev;
-		Node* newnode = new Node(x);
-
-		tail->_next = newnode;
-		newnode->_prev = tail;
-
-		newnode->_next = _head;
-		_head->_prev = newnode;
-
-		//Insert(end(), x);
-	}
-
-	// 头插
-	void PushFront(const T& x)
-	{
-		Insert(begin(), x);
-	}
-
-	// 尾删
-	void PopBack()
-	{
-		Erase(--end());
-	}
-
-	// 头删
-	void PopFront() 
-	{
-		Erase(begin());
-	}
-
-	// 在pos前插入
-	void Insert(iterator pos, const T& x)
-	{
-		Node* cur = pos._node;
-		Node* prev = cur->_prev;
-		Node* newnode = new Node(x);
-
-		prev->_next = newnode;
-		newnode->_prev = prev;
-
-		newnode->_next = cur;
-		cur->_prev = newnode;
-	}
-
-	// 删除pos所在结点
-	iterator Erase(iterator pos)
-	{
-		assert(pos._node != _head);
-		Node* cur = pos._node;
-		Node* prev = cur->_prev;
-		Node* next = cur->_next;
-
-		prev->_next = next;
-		next->_prev = prev;
-
-		delete cur;
-		return iterator(next);
-	}
-
-	size_t Size() 
-	{
-		size_t size = 0;
-		for (auto e : *this) 
+		Self operator++()
 		{
-			++size;
+			_node = _node->_next;
+			return *this;
 		}
-		return size; 
-	}
 
-	bool Empty() 
+		Self operator++(int)
+		{
+			Self tmp(*this);
+			_node = _node->_next;
+			return tmp;
+		}
+
+		Self operator--()
+		{
+			_node = _node->_prev;
+			return *this;
+		}
+
+		Self operator--(int)
+		{
+			Self tmp(*this);
+			_node = _node->_prev;
+			return tmp;
+		}
+
+		bool operator!=(Self it)
+		{
+			return _node != it._node;
+		}
+
+		bool operator==(Self it)
+		{
+			return _node == it._node;
+		}
+	};
+
+	template<class T>
+	class list
 	{
-		return _head->_next = _head;
-	}
+	public:
+		typedef ListIterator<T, T&, T*> iterator;
+		typedef ListIterator<T, const T&, const T*> const_iterator;
+	private:
+		typedef ListNode<T> Node;
+	public:
+		iterator begin()
+		{
+			return iterator(_head->_next);
+		}
 
-private:
-	Node* _head;
-};
+		iterator end()
+		{
+			return iterator(_head);
+		}
 
-struct AA 
-{
-	int _a1;
-	int _a2;
-};
+		const_iterator begin() const
+		{
+			return const_iterator(_head->_next);
+		}
+
+		const_iterator end() const
+		{
+			return const_iterator(_head);
+		}
+
+		list()
+			: _head(nullptr)
+		{
+			_head = new Node;
+			_head->_next = _head;
+			_head->_prev = _head;
+		}
+
+		~list()
+		{
+			clear();
+			delete _head;
+			_head = nullptr;
+		}
+
+		//拷贝构造
+		list(const list<T>& l)
+			: _head(nullptr)
+		{
+			_head = new Node;
+			_head->_next = _head;
+			_head->_prev = _head;
+
+			for (auto e : l)
+			{
+				push_back(e);
+			}
+		}
+
+		//赋值运算符传统写法
+		//list<T>& operator=(const list<T>& l)
+		//{
+		//	if (this != &l)
+		//	{
+		//		clear();
+		//		for (auto e : l)
+		//		{
+		//			push_back(e);
+		//		}
+		//	}
+		//	return *this;
+		//}
+
+		//赋值运算符现代写法
+		list<T>& operator=(list<T> l)
+		{
+			std::swap(_head, l._head);
+			return *this;
+		}
+
+		void clear()
+		{
+			iterator it = begin();
+			while (it != end())
+			{
+				erase(it++);
+			}
+		}
+
+		void push_back(const T& val)
+		{
+			Node* tail = _head->_prev;
+			Node* new_node = new Node(val);
+			tail->_next = new_node;
+			new_node->_prev = tail;
+			_head->_prev = new_node;
+			new_node->_next = _head;
+		}
+
+		void pop_back()
+		{
+			assert(_head->_prev != nullptr);
+			Node* tail = _head->_prev;
+			Node* prev = tail->_prev;
+			delete tail;
+			_head->_prev = prev;
+			prev->_next = _head;
+		}
+
+		void push_front(const T& val)
+		{
+			Node* cur = _head->_next;
+			Node* new_node = new Node(val);
+			_head->_next = new_node;
+			new_node->_prev = _head;
+			new_node->_next = cur;
+			cur->_prev = new_node;
+		}
+
+		void pop_front()
+		{
+			assert(_head->_next != nullptr);
+			Node* cur = _head->_next;
+			Node* next = cur->_next;
+			delete cur;
+			_head->_next = next;
+			next->_prev = _head;
+		}
+
+		iterator insert(iterator pos, const T& val)
+		{
+			Node* cur = pos._node;
+			Node* prev = cur->_prev;
+			Node* new_node = new Node(val);
+			prev->_next = new_node;
+			new_node->_prev = prev;
+			new_node->_next = cur;
+			cur->_prev = new_node;
+			return pos;
+		}
+
+		iterator erase(iterator pos)
+		{
+			assert(pos != end());
+			Node* cur = pos._node;
+			Node* prev = cur->_prev;
+			Node* next = cur->_next;
+			delete cur;
+			prev->_next = next;
+			next->_prev = prev;
+			return iterator(next);
+		}
+	private:
+		Node* _head;
+	};
+}
