3f9573943d4774d3005cf4c8dba931cde643b567
achen228 <achen228@163.com>
2020-08-02 10:58:00 +0800

    Create Test.cpp

---
+++ C++11/函数调用链中异常/Test.cpp
@@ -0,0 +1,37 @@
+//函数调用链中异常
+double Division(int a, int b)
+{
+    // 当b == 0时抛出异常
+    if (b == 0)
+    {
+        throw "Division by zero condition!";
+    }        
+    else
+    {
+        return ((double)a / (double)b);
+    }      
+}
+
+void Func()
+{
+    int len, time;
+    cin >> len >> time;
+    cout << Division(len, time) << endl;
+}
+
+int main()
+{
+    try
+    {
+        Func();
+    }
+    catch (const char* errmsg)
+    {
+        cout << errmsg << endl;
+    }
+    catch (...)
+    {
+        cout << "unkown exception" << endl;
+    }
+    return 0;
+}
